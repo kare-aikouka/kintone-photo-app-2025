9edadb43998bbcc7f0e17b881242d22461e986a6
kare-aikouka <t.kikuchi@aizawa-group.co.jp>
2025-06-26 22:25:58 +0900

    Enable Bootstrap and initialize tooltip

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,11 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+// Bootstrapの動作確認用（ツールチップを有効化）
+document.addEventListener("DOMContentLoaded", () => {
+  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})
