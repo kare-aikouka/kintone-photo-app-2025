ae187173e12dc3d23817ef0ed1925ba103ab627c
kare-aikouka <t.kikuchi@aizawa-group.co.jp>
2025-06-26 22:34:17 +0900

    Fix: use bootstrap.Tooltip

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,6 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 
-// Bootstrapの動作確認用（ツールチップを有効化）
 document.addEventListener("DOMContentLoaded", () => {
   const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
   tooltipTriggerList.map(function (tooltipTriggerEl) {
