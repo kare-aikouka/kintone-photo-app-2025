c80ae436208cf0ee1dcbf88f2a12ef2c3ab42258
kare-aikouka <t.kikuchi@aizawa-group.co.jp>
2025-06-28 22:55:45 +0900

    fix: remove KintoneSync::Machines reference

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -2,7 +2,5 @@ class MachinesController < ApplicationController
   before_action :authentication
 
   def index
-    # Kintone連携のロジック例（kintone_sync等のクラスはプロジェクト固有なのでご自身の環境に合わせて修正）
-    @machines = ::KintoneSync::Machines.all  # ←既存のkintone連携クラスを利用
   end
 end
